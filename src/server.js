4e909226be8d3363d2dcf2bde83927ad6fbb5604
Sharad Aghara <sharadaghara24@gmail.com>
2024-06-15 20:16:23 +0530

    Database connection implemanted

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const { URL, PORT } = require('./config/index');
+const mongoose = require('mongoose');
+
+// connect to mongoDB
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(console.log("Connection with database is successful."))
+    .catch((err) => console.log("error to connect to Database: ", err));
 
 // app start
 app.listen(PORT, () => {
