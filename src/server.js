bc309b264fae3b449bb8e1febee520c5e0c2fb1e
Sharad Aghara <sharadaghara24@gmail.com>
2024-06-15 17:33:28 +0530

    Implemented server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
+const { URL, PORT } = require('./config/index');
 
+// app start
+app.listen(PORT, () => {
+    console.log(`App is running on PORT: ${PORT}`);
+})
