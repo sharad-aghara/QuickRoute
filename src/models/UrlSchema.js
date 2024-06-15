50780cde75ca52db1b89a83903fd664812df4156
Sharad Aghara <sharadaghara24@gmail.com>
2024-06-15 17:33:43 +0530

    Created UrlSchema model

--- src/models/UrlSchema.js
+++ src/models/UrlSchema.js
@@ -5,7 +5,21 @@ const UrlSchema = new mongoose.Schema(
         shortId: {
             type: String,
             required: true,
-            
-        }
+            unique: true
+        },
+        redirectUrl: {
+            type: String,
+            required: true
+        },
+        visitHistory: [
+            { timestamp: { type: Number } }
+        ],
+    },
+    {
+        timestamps: true
     }
-)
+);
+
+const URL = mongoose.model('url', UrlSchema);
+
+module.exports = URL;
