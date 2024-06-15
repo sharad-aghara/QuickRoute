fdf756c61ff2310f5a6c2a8f79f99ff8c498967b
Sharad Aghara <sharadaghara24@gmail.com>
2024-06-15 23:37:48 +0530

    Implemanted main route to server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
-const { URL, PORT } = require('./config/index');
+const { URI, PORT } = require('./config/index');
 const mongoose = require('mongoose');
+const urlRoute = require('./routes/routes');
 
 // connect to mongoDB
-mongoose.connect(URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(URI)
     .then(console.log("Connection with database is successful."))
     .catch((err) => console.log("error to connect to Database: ", err));
+
+// connect main route to server
+app.use('/url', urlRoute);
 
 // app start
 app.listen(PORT, () => {
